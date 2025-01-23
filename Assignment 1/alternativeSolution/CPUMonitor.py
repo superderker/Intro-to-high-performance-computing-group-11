7dad861549b84cfe721879a437fc7351ce7167e2
ashhan <ashhan@kth.se>
2025-01-23 14:26:38 +0100

    Add alternative solution

---
+++ Assignment 1/alternativeSolution/CPUMonitor.py
@@ -0,0 +1,34 @@
+from time import perf_counter as time
+import subprocess
+import psutil
+import sys
+import threading 
+import numpy as np
+from functools import wraps
+
+def collectUtil(fn):
+    @wraps(fn)
+    def measure(*args, **kwargs):
+        def collect(event, args):
+                f=open("test.dat", "w")
+                measurements=np.array([0]*psutil.cpu_count(logical=False))
+                for i in measurements:
+                    f.write(str(i) + " ")
+                f.write(f"{time()} \n")
+                while not event.is_set():
+                    perc=psutil.cpu_percent(interval=0.25, percpu=True)
+                    for i in perc:
+                        f.write(str(i) + " ")
+                    f.write(f"{time()} \n")
+                f.close()
+                return
+        stopEvent=threading.Event()
+        t=threading.Thread(target=collect, args=(stopEvent, False))
+        t.start()
+        result=fn(*args, **kwargs)
+        stopEvent.set()
+        t.join()
+        subprocess.Popen([sys.executable, "PlotCPU.py", "test.dat"])
+        return result
+    return measure
+
---
+++ Assignment 1/alternativeSolution/PlotCPU.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+def doPlot(filename):
+    data=np.loadtxt(filename)
+    shape=np.shape(data)
+    startval=data[:,-1][0]
+    data[:,-1]=data[:,-1]-startval
+    print("\nCore usage in percentage")
+    for i in range(shape[1]-1):
+        string="Core " + str(i+1)
+        print(f"{string:<11}", end='', flush=True)
+    print(f"{"time":<11}")
+
+    def mapFunc(val):
+        return f"{val:<10.5}"
+    for i in range(shape[0]):
+        res=map(mapFunc, data[i,:])
+        print(*res)
+    plt.figure(figsize=(10,6))
+    for i in range(shape[1]-1):
+        plt.plot(data[:,-1],data[:,i], label=f"Core {i+1}")
+    plt.legend(loc='upper left', bbox_to_anchor=(1, 1))
+    plt.grid()
+    plt.axis([None, None, 0, 100])
+    plt.show()
+
+
+   
+
+if __name__ == "__main__":
+    doPlot(sys.argv[1])
