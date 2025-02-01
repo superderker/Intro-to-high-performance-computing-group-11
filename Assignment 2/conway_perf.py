a61172c5be1513469d5fdd843c4b53d9d7f1b4ad
yolk-pie-L <493577282@qq.com>
2025-02-01 13:25:07 +0100

    add task B.2

--- Assignment 2/conway_perf.py
+++ Assignment 2/conway_perf.py
@@ -1,5 +1,16 @@
+"""
+conway.py 
+
+A simple Python/matplotlib implementation of Conway's Game of Life.
+
+Author: Mahesh Venkitachalam
+"""
+
 import sys, argparse
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import timeit
 
 ON = 255
 OFF = 0
@@ -20,8 +31,10 @@ def addGlider(i, j, grid):
 def addGosperGliderGun(i, j, grid):
     """adds a Gosper Glider Gun with top left cell at (i, j)"""
     gun = np.zeros(11 * 38).reshape(11, 38)
+
     gun[5][1] = gun[5][2] = 255
     gun[6][1] = gun[6][2] = 255
+
     gun[3][13] = gun[3][14] = 255
     gun[4][12] = gun[4][16] = 255
     gun[5][11] = gun[5][17] = 255
@@ -29,6 +42,7 @@ def addGosperGliderGun(i, j, grid):
     gun[7][11] = gun[7][17] = 255
     gun[8][12] = gun[8][16] = 255
     gun[9][13] = gun[9][14] = 255
+
     gun[1][25] = 255
     gun[2][23] = gun[2][25] = 255
     gun[3][21] = gun[3][22] = 255
@@ -36,16 +50,22 @@ def addGosperGliderGun(i, j, grid):
     gun[5][21] = gun[5][22] = 255
     gun[6][23] = gun[6][25] = 255
     gun[7][25] = 255
+
     gun[3][35] = gun[3][36] = 255
     gun[4][35] = gun[4][36] = 255
+
     grid[i : i + 11, j : j + 38] = gun
 
 
-def update(grid, N):
-    """Update the grid based on Conway's rules"""
+def update(frameNum, img, grid, N):
+    # copy grid since we require 8 neighbors for calculation
+    # and we go line by line
     newGrid = grid.copy()
     for i in range(N):
         for j in range(N):
+            # compute 8-neghbor sum
+            # using toroidal boundary conditions - x and y wrap around
+            # so that the simulaton takes place on a toroidal surface.
             total = int(
                 (
                     grid[i, (j - 1) % N]
@@ -59,41 +79,77 @@ def update(grid, N):
                 )
                 / 255
             )
+            # apply Conway's rules
             if grid[i, j] == ON:
                 if (total < 2) or (total > 3):
                     newGrid[i, j] = OFF
             else:
                 if total == 3:
                     newGrid[i, j] = ON
-    return newGrid
+    # update data
+    img.set_data(newGrid)
+    grid[:] = newGrid[:]
+    return (img,)
 
 
+# main() function
 def main():
+    # Command line args are in sys.argv[1], sys.argv[2] ..
+    # sys.argv[0] is the script name itself and can be ignored
+    # parse arguments
     parser = argparse.ArgumentParser(
         description="Runs Conway's Game of Life simulation."
     )
-    parser.add_argument("--grid-size", dest="N", required=False, type=int, default=100)
-    parser.add_argument("--steps", dest="steps", required=False, type=int, default=100)
+    # add arguments
+    parser.add_argument("--grid-size", dest="N", required=False)
+    parser.add_argument("--mov-file", dest="movfile", required=False)
+    parser.add_argument("--interval", dest="interval", required=False)
     parser.add_argument("--glider", action="store_true", required=False)
     parser.add_argument("--gosper", action="store_true", required=False)
     args = parser.parse_args()
 
-    N = args.N
-    steps = args.steps
+    # set grid size
+    N = 100
+    if args.N and int(args.N) > 8:
+        N = int(args.N)
 
-    grid = np.zeros((N, N))
+    # set animation update interval
+    updateInterval = 50
+    if args.interval:
+        updateInterval = int(args.interval)
+
+    # declare grid
+    grid = np.array([])
+    # check if "glider" demo flag is specified
     if args.glider:
+        grid = np.zeros(N * N).reshape(N, N)
         addGlider(1, 1, grid)
     elif args.gosper:
+        grid = np.zeros(N * N).reshape(N, N)
         addGosperGliderGun(10, 10, grid)
     else:
+        # populate grid with random on/off - more off than on
         grid = randomGrid(N)
 
-    for _ in range(steps):
-        grid = update(grid, N)
+    # # set up animation
+    # fig, ax = plt.subplots()
+    # img = ax.imshow(grid, interpolation='nearest')
+    # ani = animation.FuncAnimation(fig, update, fargs=(img, grid, N, ),
+    #                               frames = 10,
+    #                               interval=updateInterval,
+    #                               save_count=50)
 
-    print("Simulation complete.")
+    # # # of frames?
+    # # set output file
+    # if args.movfile:
+    #     ani.save(args.movfile, fps=30, extra_args=['-vcodec', 'libx264'])
+
+    # plt.show()
 
 
+# call main
 if __name__ == "__main__":
+    start = timeit.default_timer()
     main()
+    stop = timeit.default_timer()
+    print("Time: ", stop - start)
