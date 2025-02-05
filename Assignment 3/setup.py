d9c42762c4dd0fe4c90ffe3249168e59e4242a0b
ashhan <ashhan@kth.se>
2025-02-05 19:45:04 +0100

    fix setup file

--- Assignment 3/setup.py
+++ Assignment 3/setup.py
@@ -2,5 +2,7 @@ from setuptools import setup
 from Cython.Build import cythonize
 import numpy
 
-setup(ext_modules=cythonize("cythonfnOpt.pyx", "cythonfn.pyx",compiler_directives={"language_level":"3"}),
+setup(ext_modules=cythonize("cythonfnOpt.pyx",compiler_directives={"language_level":"3"}),
+      include_dirs=[numpy.get_include()])
+setup(ext_modules=cythonize("cythonfn.pyx",compiler_directives={"language_level":"3"}),
       include_dirs=[numpy.get_include()])
